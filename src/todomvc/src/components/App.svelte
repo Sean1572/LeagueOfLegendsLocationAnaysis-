095ea2c58b806ac41b3c11113df9abec7f25bc82
Sean Perry <sean.hyatt.perry@gmail.com>
2024-02-10 15:44:08 -0800

    add opacity map code

--- src/todomvc/src/components/App.svelte
+++ src/todomvc/src/components/App.svelte
@@ -3,22 +3,25 @@
     import HeatMap from '../components/HeatMap.svelte';
     import * as d3 from 'd3';
     
-    data = [];
+    let data = [];
 
     onMount(async () => {
-        d3.csv("test.csv", function(row) {
+        await d3.csv("test1.csv", function(row) {
             data.push({
-                "match_ids": row["match_ids]
+                "x": row['x'],
+                "y": row['y']
             })
         });
+        data=data; //???????
     })
 
-    $: console.log(data);
+    
 </script>
 
 <main>
 
     <h1>testing</h1>
-    <HeatMap />
+    <HeatMap bind:data={data} />
+    <HeatMap bind:data={data} />
 
 </main>
