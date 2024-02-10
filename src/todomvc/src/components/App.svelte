99b6f7b4462b4fce6eb6f64263c0d3444c9f1626
Sean Perry <sean.hyatt.perry@gmail.com>
2024-02-10 10:46:33 -0800

    Adding building block for infrastructure

---
+++ src/todomvc/src/components/App.svelte
@@ -0,0 +1,10 @@
+<script>
+    import HeatMap from '../components/HeatMap.svelte';
+</script>
+
+<main>
+
+    <h1>testing</h1>
+    <HeatMap bind:data={[]}/>
+
+</main>
---
+++ src/todomvc/src/components/HeatMap.svelte
@@ -0,0 +1,10 @@
+<script>
+	import * as d3 from 'd3';
+    export let data = [];
+</script>
+
+<div class="visualization">
+    <svg width="500" height="500">
+        <circle r="45" cx="50" cy="50" stroke="green" stroke-width="3" fill="red" />
+    </svg>
+</div>
